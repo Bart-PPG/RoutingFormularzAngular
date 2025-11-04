700411d78d63624f566f6eef3d04b453ae3caa11
bart-ppg <bartek.nitek@onet.pl>
2025-11-04 08:50:45 +0100

    filmy

--- src/app/filmy/filmy.component.ts
+++ src/app/filmy/filmy.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filmy',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './filmy.component.html',
   styleUrl: './filmy.component.css'
 })
 export class FilmyComponent {
+tytul = ""
+rodzaj = ""
 
+dodajFilm(){
+  console.log("tytuł: "+this.tytul+" Rodzaj: "+ this.rodzaj)
+}
 }
