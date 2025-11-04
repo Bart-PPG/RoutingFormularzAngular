0e890eba92ea69094f6a4d8b1b3a44593aea9c7f
bart-ppg <bartek.nitek@onet.pl>
2025-11-04 09:31:53 +0100

    ciagi

--- src/app/ciag/ciag.component.ts
+++ src/app/ciag/ciag.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ciag',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './ciag.component.html',
   styleUrl: './ciag.component.css'
 })
 export class CiagComponent {
+  tablicaLiczb:number[] = []
+  liczba = 0
+  roznica = 0
+  liczbaWyrazowCiagu = 0
 
+  ciag(){
+    this.tablicaLiczb.push(this.liczba);
+    for(let i = 1;i < this.liczbaWyrazowCiagu;i++){
+      this.tablicaLiczb[i]= this.tablicaLiczb[i-1]+this.roznica
+    }
+
+  }
 }
